c86d57805b103bf88d790b1ca17f7cb299841734
B Spektor <20343235+BSpwr@users.noreply.github.com>
2023-08-13 20:31:45 -0400

    Initial commit for megakill

---
+++ megakill/configuration.nix
@@ -0,0 +1,170 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./zfs.nix
+      ./persist.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+ # boot.loader.systemd-boot.enable = true;
+ # boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+  programs.zsh.enable = true;
+  environment.shells = with pkgs; [ zsh ]; 
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     tree
+  #   ];
+  # };
+
+  users.users.bree = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
+    hashedPassword = "$6$juxnzab1rsHBPVSz$HmoxRiTcbUnUnQ.HbksYniCn5Gdh2fXFFF58J.YCyZhxeIwSzV0aStTgxLC.YOifnxOcyYCsrzvanOq9d7Pl/.";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   nixpkgs.config.allowUnfree = true;
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     neovim
+     wget
+     git
+     firefox
+     killall
+     tree
+     vscode
+     vscodium
+     bitwarden
+     chromium
+     spotify
+     discord
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
---
+++ megakill/persist.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, lib, ... }: {
+
+  # Persist these paths across boots
+  fileSystems."/etc/nixos" = {
+    device = "/home/bree/nixos-configuration/megakill";
+    options = [ "bind" ];
+  };
+  fileSystems."/etc/NetworkManager" = {
+    device = "/persist/etc/NetworkManager";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/bluetooth" = {
+    device = "/persist/var/lib/bluetooth";
+    options = [ "bind" ];
+  };
+  fileSystems."/etc/ssh" = {
+    device = "/persist/etc/ssh";
+    options = [ "bind" ];
+  };
+  fileSystems."/etc/cockpit" = {
+    device = "/persist/etc/cockpit";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/log" = {
+    device = "/persist/var/log";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/tailscale" = {
+    device = "/persist/var/lib/tailscale";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/db/sudo" = {
+    device = "/persist/var/db/sudo";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/lxc" = {
+    device = "/persist/var/lib/lxc";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/lxd" = {
+    device = "/persist/var/lib/lxd";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/cni" = {
+    device = "/persist/var/lib/cni";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/machines" = {
+    device = "/persist/var/lib/machines";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/containers" = {
+    device = "/persist/var/lib/containers";
+    options = [ "bind" ];
+  };
+  fileSystems."/var/lib/clamav" = {
+    device = "/persist/var/lib/clamav";
+    options = [ "bind" ];
+  };
+
+  # loginctl-linger -- this enables “lingering” for selected users
+  # inspired by the discussion (and linked code) in https://github.com/NixOS/nixpkgs/issues/3702
+  # this should just be a NixOS option really
+  fileSystems."/var/lib/systemd/linger" = {
+    device = "/persist/var/lib/systemd/linger";
+    options = [ "bind" ];
+  };
+
+  # machine-id is used by systemd for the journal
+  # this allows to use journalctl to look at journals for previous boots
+  environment.etc."machine-id".source = "/persist/etc/machine-id";
+
+  # kernel modules are exposed at /lib/modules
+  fileSystems."/lib/modules" = {
+    device = "/run/booted-system/kernel-modules/lib/modules";
+    options = [ "bind" ];
+  };
+
+}
