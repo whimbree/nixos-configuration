798761358203a2e9812761ccdf45745fe3db8f10
Bree Spektor <20343235+BSpwr@users.noreply.github.com>
2023-10-06 17:40:09 -0400

    redockerize nfs, make megakill use nfs/samba directly for speed

--- megakill/bastion-nas.nix
+++ megakill/bastion-nas.nix
@@ -3,39 +3,48 @@
 {
   # NFS
   fileSystems."/mnt/backup" = {
-    device = "bastion:/export/backup/megakill";
+    device = "192.168.69.59:/backup/megakill";
     fsType = "nfs";
     options = [
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=1800"
+      "rsize=131072"
+      "wsize=131072"
+      "sync"
     ];
   };
 
   fileSystems."/home/bree/nas" = {
-    device = "bastion:/export/nas/bree";
+    device = "192.168.69.59:/nas/bree";
     fsType = "nfs";
     options = [
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=1800"
+      "rsize=131072"
+      "wsize=131072"
+      "sync"
     ];
   };
 
   fileSystems."/mnt/images" = {
-    device = "bastion:/export/images";
+    device = "192.168.69.59:/images";
     fsType = "nfs";
     options = [
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=1800"
+      "rsize=131072"
+      "wsize=131072"
+      "sync"
     ];
   };
 
   # Samba
   environment.systemPackages = [ pkgs.cifs-utils ];
   fileSystems."/mnt/media" = {
-    device = "//bastion/media";
+    device = "//192.168.69.59/media";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
@@ -45,7 +54,7 @@
   };
 
   fileSystems."/mnt/downloads" = {
-    device = "//bastion/downloads";
+    device = "//192.168.69.59/downloads";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
@@ -55,7 +64,7 @@
   };
 
   fileSystems."/mnt/public" = {
-    device = "//bastion/public";
+    device = "//192.168.69.59/public";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
