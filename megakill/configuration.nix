deb75d4d89b6edcb342b60a1966b71de9c60a76f
B Spektor <20343235+BSpwr@users.noreply.github.com>
2023-10-27 11:52:43 -0400

    use cloudflare DNS over TLS

--- megakill/configuration.nix
+++ megakill/configuration.nix
@@ -26,6 +26,17 @@
   systemd.network.enable = true;
   systemd.network.wait-online.enable = lib.mkForce false;
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
 
   # Set your time zone.
   time.timeZone = "America/New_York";
--- wheatley/configuration.nix
+++ wheatley/configuration.nix
@@ -17,6 +17,17 @@
   networking.hostName = "wheatley";
   networking.firewall.enable = true;
   networking.enableIPv6 = false;
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
 
   time.timeZone = "America/New_York";
 
