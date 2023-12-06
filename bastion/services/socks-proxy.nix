d0eadfa33f8d60c3b4bcd285a330bd7c288e795c
B Spektor <20343235+BSpwr@users.noreply.github.com>
2023-12-05 19:26:31 -0500

    socks-proxy use host networking

--- bastion/services/socks-proxy.nix
+++ bastion/services/socks-proxy.nix
@@ -1,29 +1,12 @@
 { config, pkgs, lib, ... }: {
 
-  systemd.services.docker-create-network-socks-proxy = {
-    enable = true;
-    description = "Create socks-proxy docker network";
-    path = [ pkgs.docker ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = "yes";
-      ExecStart = pkgs.writeScript "docker-create-network-socks-proxy" ''
-        #! ${pkgs.runtimeShell} -e
-        ${pkgs.docker}/bin/docker network create socks-proxy || true
-      '';
-    };
-    after = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-  };
-
   virtualisation.oci-containers.containers."socks-proxy" = {
     autoStart = true;
     image = "ghcr.io/whimbree/microsocks:latest";
-    ports = [ "100.64.0.2:1080:1080" ]; # expose only to tailscale
-    dependsOn = [ "create-network-socks-proxy" ];
+    environment = { PROXY_PORT = "1080"; };
     extraOptions = [
       # networks
-      "--network=socks-proxy"
+      "--network=host"
     ];
   };
 
--- wheatley/services/socks-proxy.nix
+++ wheatley/services/socks-proxy.nix
@@ -3,6 +3,7 @@
   virtualisation.oci-containers.containers."socks-proxy" = {
     autoStart = true;
     image = "ghcr.io/whimbree/microsocks:latest";
+    environment = { PROXY_PORT = "1080"; };
     extraOptions = [
       # networks
       "--network=host"
