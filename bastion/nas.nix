43976db0e08380823dcf0e22321b310ad1925798
B Spektor <20343235+BSpwr@users.noreply.github.com>
2025-03-28 01:54:27 -0400

    update sambda config for 24.11

--- bastion/nas.nix
+++ bastion/nas.nix
@@ -4,40 +4,39 @@
     enable = true;
     openFirewall = true;
     securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server role = standalone server
-      dns proxy = no
-      vfs objects = catia streams_xattr
-      pam password change = yes
-      map to guest = bad user
-      usershare allow guests = yes
-      create mask = 0664
-      force create mode = 0664
-      directory mask = 0775
-      force directory mode = 0775
-      follow symlinks = yes
-      load printers = no
-      printing = bsd
-      printcap name = /dev/null
-      disable spoolss = yes
-      strict locking = no
-      aio read size = 0
-      aio write size = 0
-      vfs objects = acl_xattr catia streams_xattr
-      inherit permissions = yes
-      # Security
-      client ipc max protocol = SMB3
-      client ipc min protocol = SMB2_10
-      client max protocol = SMB3
-      client min protocol = SMB2_10
-      server max protocol = SMB3
-      server min protocol = SMB2_10
-    '';
-    shares = {
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server role" = "standalone server";
+        "dns proxy" = "no";
+        "pam password change" = "yes";
+        "map to guest" = "bad user";
+        "usershare allow guests" = "yes";
+        "create mask" = "0664";
+        "force create mode" = "0664";
+        "directory mask" = "0775";
+        "force directory mode" = "0775";
+        "follow symlinks" = "yes";
+        "load printers" = "no";
+        "printing" = "bsd";
+        "printcap name" = "/dev/null";
+        "disable spoolss" = "yes";
+        "strict locking" = "no";
+        "aio read size" = "0";
+        "aio write size" = "0";
+        "vfs objects" = "acl_xattr catia streams_xattr";
+        "inherit permissions" = "yes";
+        # Security
+        "client ipc max protocol" = "SMB3";
+        "client ipc min protocol" = "SMB2_10";
+        "client max protocol" = "SMB3";
+        "client min protocol" = "SMB2_10";
+        "server max protocol" = "SMB3";
+        "server min protocol" = "SMB2_10";
+      };
       public = {
-        path = "/ocean/public";
-        browseable = "yes";
+        "path" = "/ocean/public";
+        "browseable" = "yes";
         # This is public, everybody can access.
         "read only" = "yes";
         "guest ok" = "yes";
@@ -51,9 +50,9 @@
           "/.apdisk/.DS_Store/.TemporaryItems/.Trashes/desktop.ini/ehthumbs.db/Network Trash Folder/Temporary Items/Thumbs.db/";
         "delete veto files" = "yes";
       };
-      downloads = {
-        path = "/ocean/downloads";
-        browseable = "yes";
+        downloads = {
+        "path" = "/ocean/downloads";
+        "browseable" = "yes";
         "read only" = "yes";
         "guest ok" = "no";
         "force user" = "fileshare";
@@ -67,8 +66,8 @@
         "delete veto files" = "yes";
       };
       media = {
-        path = "/merged/media";
-        browseable = "yes";
+        "path" = "/merged/media";
+        "browseable" = "yes";
         "read only" = "yes";
         "guest ok" = "no";
         "force user" = "fileshare";
