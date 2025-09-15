b8558baf3465ee939af0ba82fd788b1025e47a27
B Spektor <20343235+BSpwr@users.noreply.github.com>
2025-09-14 23:51:15 -0400

    change microvm defaults

--- bastion/modules/microvm-defaults.nix
+++ bastion/modules/microvm-defaults.nix
@@ -55,9 +55,21 @@
   };
 
   # Default networking configuration
+  systemd.network.enable = true; # VMs use systemd-networkd
   networking = {
     enableIPv6 = false; # Disable IPv6 globally
     useNetworkd = lib.mkDefault true; # VMs use systemd-networkd
+  };
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
   };
 
   # User management optimized for tmpfs
