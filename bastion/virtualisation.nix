262465140be57973c34c6168b574f9414dd003fb
B Spektor <20343235+BSpwr@users.noreply.github.com>
2025-11-02 21:20:05 -0500

    use podman on bastion

--- bastion/virtualisation.nix
+++ bastion/virtualisation.nix
@@ -1,20 +1,27 @@
 { pkgs, ... }: {
   virtualisation = {
-    # enable docker
-    docker = {
+    containers.enable = true;
+    podman = {
       enable = true;
-      autoPrune.enable = true;
-      storageDriver = "zfs";
-      liveRestore = true;
-      daemon.settings = {
-        default-address-pools = [{
-          base = "172.17.0.0/12";
-          size = 20;
-        }];
-        ip = "127.0.0.1";
-      };
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
-    oci-containers.backend = "docker";
+    oci-containers.backend = "podman";
+    # enable docker
+    # docker = {
+    #   enable = true;
+    #   autoPrune.enable = true;
+    #   storageDriver = "zfs";
+    #   liveRestore = true;
+    #   daemon.settings = {
+    #     default-address-pools = [{
+    #       base = "172.17.0.0/12";
+    #       size = 20;
+    #     }];
+    #     ip = "127.0.0.1";
+    #   };
+    # };
+
     # enable libvirt
     libvirtd.enable = true;
     # enable LXD
