8cfd41ad7b828a350dc89f993bad763f7ca01cf8
B Spektor <20343235+BSpwr@users.noreply.github.com>
2026-04-11 12:05:59 -0400

    Update persist.nix to include fsType = "none" for various file systems and remove unused bindings for lxc, lxd, cni, machines, and containers, streamlining the configuration.

--- bastion/persist.nix
+++ bastion/persist.nix
@@ -2,54 +2,41 @@
   # Persist these paths across boots
   fileSystems."/etc/nixos" = {
     device = "/home/bree/nixos-configuration";
+    fsType = "none";
     options = [ "bind" ];
   };
   fileSystems."/var/lib/samba" = {
     device = "/persist/var/lib/samba";
+    fsType = "none";
     options = [ "bind" ];
   };
   fileSystems."/var/lib/tailscale" = {
     device = "/persist/var/lib/tailscale";
+    fsType = "none";
     options = [ "bind" ];
   };
   fileSystems."/var/db/sudo" = {
     device = "/persist/var/db/sudo";
-    options = [ "bind" ];
-  };
-  fileSystems."/var/lib/lxc" = {
-    device = "/persist/var/lib/lxc";
-    options = [ "bind" ];
-  };
-  fileSystems."/var/lib/lxd" = {
-    device = "/persist/var/lib/lxd";
-    options = [ "bind" ];
-  };
-  fileSystems."/var/lib/cni" = {
-    device = "/persist/var/lib/cni";
-    options = [ "bind" ];
-  };
-  fileSystems."/var/lib/machines" = {
-    device = "/persist/var/lib/machines";
-    options = [ "bind" ];
-  };
-  fileSystems."/var/lib/containers" = {
-    device = "/persist/var/lib/containers";
+    fsType = "none";
     options = [ "bind" ];
   };
   fileSystems."/var/lib/clamav" = {
     device = "/persist/var/lib/clamav";
+    fsType = "none";
     options = [ "bind" ];
   };
   fileSystems."/root" = {
     device = "/persist/root";
+    fsType = "none";
     options = [ "bind" ];
   };
 
-  # loginctl-linger -- this enables “lingering” for selected users
+  # loginctl-linger -- this enables "lingering" for selected users
   # inspired by the discussion (and linked code) in https://github.com/NixOS/nixpkgs/issues/3702
   # this should just be a NixOS option really
   fileSystems."/var/lib/systemd/linger" = {
     device = "/persist/var/lib/systemd/linger";
+    fsType = "none";
     options = [ "bind" ];
   };
 
@@ -70,6 +57,7 @@
   # kernel modules are exposed at /lib/modules
   fileSystems."/lib/modules" = {
     device = "/run/booted-system/kernel-modules/lib/modules";
+    fsType = "none";
     options = [ "bind" ];
   };
 }
